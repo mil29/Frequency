1ca64061ed8dc878427201313d0a6c544b396da6
mil29 <74261419+mil29@users.noreply.github.com>
2021-06-25 19:16:24 +0100

    update

--- frequency/users/models.py
+++ frequency/users/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
 
-class Profile(models.Model):
-    user = models.OneToOneField()
+    def __str__(self):
+        return self.username
+
