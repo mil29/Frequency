dbd081d3835f58866168b26481a66eb4b8ae3adb
mil29 <74261419+mil29@users.noreply.github.com>
2021-06-28 18:36:50 +0100

    update

---
+++ frequency/users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm 
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = User      
+        fields = ['email']
+
+
--- frequency/users/views.py
+++ frequency/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+	if request.method == "POST":
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("home")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = CustomUserCreationForm
+	return render (request=request, template_name="users/register.html", context={"form":form})
+
