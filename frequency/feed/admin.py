b9a99dcb10225b738c82d51849eb068788959023
mil29 <74261419+mil29@users.noreply.github.com>
2021-07-01 13:32:56 +0100

    update

--- frequency/feed/admin.py
+++ frequency/feed/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Instrument, EQ, Track
+
+admin.site.register(Instrument)
+admin.site.register(EQ)
+admin.site.register(Track)
+
 
 
 
--- frequency/feed/models.py
+++ frequency/feed/models.py
@@ -1,3 +1,46 @@
+from decimal import Decimal
 from django.db import models
+from django.utils.datetime_safe import datetime
 
 
+
+class Instrument(models.Model):
+    title     = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now=True)
+    track     = models.ForeignKey('Track', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'title:{} track:{}'.format(self.title, self.track)
+
+
+class EQ(models.Model):
+    frequency  = models.DecimalField(decimal_places=3, max_digits=20)
+    instrument = models.ForeignKey(Instrument,on_delete=models.CASCADE)
+    track      = models.ForeignKey('Track', on_delete=models.CASCADE)
+    boost      = models.DecimalField(decimal_places=2, max_digits=10)
+    cut        = models.DecimalField(decimal_places=2, max_digits=10)
+    updated    = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return 'frequency:{} instrument:{} track:{} boost:{} cut:{}'.format(self.frequency, self.instrument, self.track, self.boost, self.cut)
+
+
+class Track(models.Model):
+    title       = models.CharField(max_length=100)
+    description = models.TextField(max_length=250, null=True)
+    artist      = models.CharField(max_length=250)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'title:{} description:{} artist:{}'.format(self.title, self.description, self.artist)
+
+
+
+
+
+
+
+
+    
